2b9662fc0b959812a0fb31b4cca573a50d30cf68
Luiz2002 <quineche44@gmail.com>
2024-12-01 18:49:03 -0500

    collect statics added

--- core/settings.py
+++ core/settings.py
@@ -117,11 +117,10 @@ USE_TZ = True
 
 
 # Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/4.2/howto/static-files/
+# https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
-MEDIA_URL = 'media/'
-MEDIA_ROOT = BASE_DIR / 'media' 
+
 #################################################################
 # for deployment
 import os
@@ -129,7 +128,12 @@ import os
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')  # new
 STATICFILES_DIRS  = [os.path.join(BASE_DIR, 'static')]  # new
 #################################################################
-################################################################# 
+#################################################################
+# for images in models
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+#################################################################
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
